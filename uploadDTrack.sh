1345d721bb30aaaaad361aed93ccafe18b04edd0
Falk Merten <falk.merten@bol-systemhaus.de>
2025-04-30 16:50:42 +0200

    Test DTrack upload

---
+++ uploadDTrack.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+echo $VERSION
+echo $DTRACK_TOKEN
+echo $PROJECT_NAME
+#curl -X "POST" "https://dtrack-api.formular-demo.de/api/v1/bom" -H 'Content-Type: multipart/form-data' -H "X-Api-Key: $DTRACK_TOKEN" -F 'autoCreate=true' -F "projectName=$PROJECT_NAME" -F "projectVersion=$VERSION" -F 'isLatest=true' -F 'parentName=OpenPDF' -F "bom=@result.json"
